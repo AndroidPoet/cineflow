9a09731ab902f22b8452b0d01d91c59f70d1502b
Ranbir Singh <poetdroid2@gmail.com>
2026-07-11 16:51:31 +0530

    Debug-mode stack logger for nav dogfooding

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
+import 'routing/nav.dart';
 
 void main() {
+  if (kDebugMode) {
+    navStack.addListener(
+      () => debugPrint('STACK: ${navStack.keys.map((k) => k.runtimeType)}'),
+    );
+  }
   runApp(const ProviderScope(child: CineFlowApp()));
 }
